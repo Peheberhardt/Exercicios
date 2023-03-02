b934c015e4893a4f27da0d5320a9e46aee1a2ad7
Pedro H Eberhardt <pedroebehardt@gmail.com>
2023-03-02 11:53:49 -0300

    some changes

--- src/Codigos.java
+++ src/Codigos.java
@@ -7,19 +7,6 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Codigos {
-
-	public static void Ex1() {
-
-		int i = 13;
-	    int soma = 0;
-	    int k = 0;
-	    
-	    while (k < i){
-	        k = k+1;
-	        soma = soma+k;
-	    }
-	    System.out.println(soma);
-	}
 	
 	public static int Ex2(int num) {
 		if(num < 2) {
@@ -36,35 +23,43 @@ public class Codigos {
 		
 		//Exercicio 1
 		
-		//{
-			//Ex1();
-		//}
+		{
+			int i = 13;
+		    int soma = 0;
+		    int k = 0;
+		    
+		    while (k < i){
+		        k = k+1;
+		        soma = soma+k;
+		    }
+		    System.out.println(soma);
+		}
 		
 		
 		
 		
 		//Exercicio 2
-//		{
-//			int num = 13;
-//				
-//			for(int i = 0; i < num; i++) {
-//				int en = Ex2(i);
-//				if(en != num) {
-//					System.out.println(en+ "");
-//				}
-//					
-//				if(en == num) {
-//					System.out.println(en+ " - O numero pertence a sequencia");
-//				}
-//			}
-//		}
+		{
+			int num = 13;
+				
+			for(int i = 0; i < num; i++) {
+				int en = Ex2(i);
+				if(en != num) {
+					System.out.println(en+ "");
+				}
+					
+				if(en == num) {
+					System.out.println(en+ " - O numero pertence a sequencia");
+				}
+			}
+		}
 		
 		
 		
 		//Exercicio 3 
-//		{
-//			Nao consegui resolver
-//		}
+		{
+			//Nao consegui resolver
+		}
 		
 		
 		
@@ -72,35 +67,35 @@ public class Codigos {
 			
 			
 		//Exercicio 4 
-//		{	
-//			double[] percentual= new double[5];
-//			double soma = 0;
-//			double[] estados = {67836.43,36678.66,29229.88,27165.48,19849.53};
-//			
-//			for(int i = 0; i < estados.length; i++) {
-//				soma += estados[i];
-//			}
-//			
-//			for(int i = 0; i < estados.length; i++) {
-//				percentual[i] = estados[i] * 100 / soma;
-//			}
-//			
-//			for(int i = 0; i< estados.length; i++) {
-//				System.out.println();
-//				if(i == 0) {
-//					System.out.println("SP: "+Math.round(percentual[i])+"%");
-//				} else if(i == 1) {
-//					System.out.println("RJ: "+Math.round(percentual[i])+"%");
-//				}else if(i == 2) {
-//					System.out.println("MG: "+Math.round(percentual[i])+"%");
-//				}else if(i == 3) {
-//					System.out.println("ES: "+Math.round(percentual[i])+"%");
-//				}else if(i == 4) {
-//					System.out.println("Outros: "+Math.round(percentual[i])+"%");
-//				}
-//			}
-//			
-//		}
+		{	
+			double[] percentual= new double[5];
+			double soma = 0;
+			double[] estados = {67836.43,36678.66,29229.88,27165.48,19849.53};
+			
+			for(int i = 0; i < estados.length; i++) {
+				soma += estados[i];
+			}
+			
+			for(int i = 0; i < estados.length; i++) {
+				percentual[i] = estados[i] * 100 / soma;
+			}
+			
+			for(int i = 0; i< estados.length; i++) {
+				System.out.println();
+				if(i == 0) {
+					System.out.println("SP: "+Math.round(percentual[i])+"%");
+				} else if(i == 1) {
+					System.out.println("RJ: "+Math.round(percentual[i])+"%");
+				}else if(i == 2) {
+					System.out.println("MG: "+Math.round(percentual[i])+"%");
+				}else if(i == 3) {
+					System.out.println("ES: "+Math.round(percentual[i])+"%");
+				}else if(i == 4) {
+					System.out.println("Outros: "+Math.round(percentual[i])+"%");
+				}
+			}
+			
+		}
 		
 		
 		
@@ -108,18 +103,18 @@ public class Codigos {
 		
 		//Exercicio 5
 		
-//		{
-//			String frase = "Joguei videogames ontem";
-//			
-//			char[] fraseReversa = frase.toCharArray();
-//			
-//			String novaFrase;
-//			
-//			for(int i = fraseReversa.length-1; i >= 0;i--) {
-//				System.out.print(fraseReversa[i]);
-//			}
-//			
-//		}
+		{
+			String frase = "Joguei videogames ontem";
+			
+			char[] fraseReversa = frase.toCharArray();
+			
+			String novaFrase;
+			
+			for(int i = fraseReversa.length-1; i >= 0;i--) {
+				System.out.print(fraseReversa[i]);
+			}
+			
+		}
 		
 		
         
